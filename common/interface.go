63beb6d33947a190e01d7fb7ffcc0ba5a05f7251
MwlLj <731025894@qq.com>
2018-12-11 18:45:46 +0800

    新增接口

--- common/interface.go
+++ common/interface.go
@@ -2,3 +2,14 @@ package common
 
 type ICrawler interface {
 }
+
+type IQueueCallback interface {
+	OnPrepareQueuePop(url []byte)
+}
+
+type IQueueMgr interface {
+	NewQueue(callback IQueueCallback) IQueue
+}
+
+type IQueue interface {
+}
